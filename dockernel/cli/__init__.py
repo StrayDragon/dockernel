4fad2e332a40a6fbb72e625a9d2020f31e60492f
L8ng <straydragonl@foxmail.com>
2023-04-11 17:00:58 +0800

    feat: add install --list option to list installed kernelspec

--- dockernel/cli/__init__.py
+++ dockernel/cli/__init__.py
@@ -1,11 +1,14 @@
-from .main import arguments as main_arguments, set_subcommand_func, run_subcommand
 from .install import arguments as install_arguments
+from .main import arguments as main_arguments
+from .main import run_subcommand, set_subcommand_func
 from .start import arguments as start_arguments
+from .uninstall import arguments as uninstall_arguments
 
-__all__ = [
+__all__ = (
     "main_arguments",
     "install_arguments",
+    "uninstall_arguments",
     "start_arguments",
     "set_subcommand_func",
     "run_subcommand",
-]
+)
---
+++ dockernel/cli/uninstall.py
@@ -0,0 +1,58 @@
+import shutil
+import sys
+from argparse import Namespace
+from pathlib import Path
+
+import rich
+
+from dockernel.cli.main import set_subcommand_func, subparsers
+
+arguments = subparsers.add_parser(
+    __name__.split(".")[-1],
+    help="Uninstall dockerized kernelspec",
+)
+arguments.add_argument(
+    "name",
+    help="the installed image_name or --name value from install cmd",
+    default="",
+)
+arguments.add_argument(
+    "--kernels-path",
+    help="kernels path to install, see https://jupyter-client.readthedocs.io/en/stable/kernels.html",
+    default=f"{sys.prefix}/share/jupyter/kernels",
+)
+arguments.add_argument(
+    "--dry-run",
+    help="not do actual operation, help for debug",
+    default=False,
+)
+
+
+def uninstall(args: Namespace) -> int:
+    name: str = args.name or ""
+    if not name:
+        raise ValueError("--name")
+
+    kernels_path_str = args.kernels_path
+    if not kernels_path_str:
+        raise ValueError("--kernels-path must not empty")
+    kernels_path = Path(kernels_path_str)
+    if not all({kernels_path.exists(), kernels_path.is_dir()}):
+        raise ValueError("--kernels-path not exist")
+
+    is_dry_run = bool(args.dry_run)
+
+    target_kernel_dir: Path = kernels_path / name
+    if not all({target_kernel_dir.exists(), target_kernel_dir.is_dir()}):
+        rich.print("[yellow]WARNING[/yellow]: not found target kernel config dir, do noting!")
+    else:
+        if is_dry_run:
+            rich.print(f"[green]OK[/green]: '{str(target_kernel_dir)}' will be uninstalled (removed)")
+        else:
+            shutil.rmtree(target_kernel_dir)
+            rich.print(f"[green]OK[/green]: '{str(target_kernel_dir)}' already uninstalled (removed)")
+
+    return 0
+
+
+set_subcommand_func(parser=arguments, func=uninstall)
